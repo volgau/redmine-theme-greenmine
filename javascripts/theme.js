968de3c6abc0dae82fe1fd0db965ad3ca4fb1563
roman-yagodin <roman.yagodin@gmail.com>
2017-10-13 15:48:45 +0300

    [WIP] Status labels

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -70,9 +70,13 @@
 var SidebarToggler = __webpack_require__ (1).SidebarToggler;
 
 (function ($, document) {
+    function wrapCellInner (selector) {
+        $(selector).wrapInner ("<span></span>");
+    }
     $(document).ready (function () {
         var sidebarToggler = new SidebarToggler ("greenmine_sidebar_state", 7);
         sidebarToggler.init ();
+        wrapCellInner ("td.status");
     });
 }) (jQuery, document);
 
