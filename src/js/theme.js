50c6c6db3a0f9c4590395e44ffe2464ae6947087
roman-yagodin <roman.yagodin@gmail.com>
2018-07-12 11:46:04 +0300

    Convert some selects to select2 if plugin is available

--- src/js/theme.js
+++ src/js/theme.js
@@ -4,10 +4,18 @@ var SidebarToggler = require ("./sidebarToggler.js").SidebarToggler;
     function wrapCellInner (selector) {
         $(selector).wrapInner ("<span></span>");
     }
+    function applySelect2 () {
+        if (typeof (window.jql) === "function") {
+            window.jql ("select#issue_assigned_to_id").select2 ({dropdownAutoWidth: 'true', width: '60%'});
+            window.jql ("select#time_entry_activity_id").select2 ();
+            window.jql ("select[id^='issue_custom_field_values']").select2 ({dropdownAutoWidth: 'true', width: '60%'});
+        }
+    }
     $(document).ready (function () {
         var sidebarToggler = new SidebarToggler ("greenmine_sidebar_state", 7);
         sidebarToggler.init ();
         wrapCellInner ("table.issues td.status");
         wrapCellInner ("div.issue div.attributes div.status div.value");
+        applySelect2 ();
     });
 }) (jQuery, document);
